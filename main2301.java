89b82a14414f7603a46fee6d00294a8947de8b3f
Vignesh <vigneshravi627@gmail.com>
2026-01-23 15:58:28 +0530

    c

---
+++ main2301.java
@@ -0,0 +1,390 @@
+// import java.util.Scanner;
+// public class main2301 {
+//     public static void main(String [] args){
+//         char c;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter a h or t:");
+//         c=in.next().charAt(0);
+//         if(c=='h'){
+//             System.out.println("you are lucky");
+//         }
+//     }
+    
+// }
+
+
+// import java.util.Scanner;
+// public class main2301 {
+//     public static void main(String [] args){
+//         int c;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter num:");
+//         c=in.nextInt();
+//         if(c>=0){
+//             System.out.println("given num is positive");
+//         }
+//     }
+    
+// }
+
+// import java.util.Scanner;
+// public class main2301 {
+//     public static void main(String [] args){
+//         int f;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter num:");
+//         f=in.nextInt();
+//         if(f%17==0){
+//             System.out.println("given num is divisible by 17");
+//         }
+//     }
+    
+// }
+
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String [] args){
+//         char g;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter char:");
+//         g=in.next().charAt(0);
+//         if(g=='a'||g=='e'||g=='i'||g=='o'||g=='u'||g=='A'||g=='E'||g=='I'||g=='O'||g=='U'){
+//             System.out.println("given char is vowel");
+//         }
+//     }
+    
+// }
+
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String [] args){
+//         int a;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter char:");
+//         a=in.nextInt();
+//         if(a%5==0&&a%7==0){
+//             System.out.println(a+" is divisible by 5&7");
+//         }
+//         else{
+//             System.out.println(a+" may not be divisibkle by 5&7");
+//         }
+//     }
+// }
+    
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String [] args){
+//         int marks;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter mark");
+//         marks=in.nextInt();
+//         if(marks>=50){
+//             System.out.println("Pass");
+//         }
+//         else{
+//             System.out.println("Fail");
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String [] args){
+//         int amount;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter amount:");
+//         amount=in.nextInt();
+//         if(amount>10000){
+//             System.out.println("debit 500");
+//         }
+//         else{
+//             System.out.println("debit 50");
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String [] args){
+//         int y;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter year:");
+//         y=in.nextInt();
+//         if((y%4==0&&y%400==0)||(y%100!=0)){
+//             System.out.println("leap year");
+//         }
+//         else{
+//             System.out.println("not a leap year");
+//         }
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String [] args){
+//         int P,A;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter Percentage");
+//         P=in.nextInt();
+//         System.out.println("Enter Attendance");
+//         A=in.nextInt();
+//         if(P>85&&A>=90){
+//             System.out.println("Distinction");
+//         }
+//         else if(P>=70&&A>=80){
+//             System.out.println("First class");
+//         }
+//         else if(P>=60&&A>=75){
+//             System.out.println("Second class");
+//         }
+//         else if(P>=50&&A>=75){
+//             System.out.println("Pass");
+//         }
+//         else{
+//             System.out.println("Fail");
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String [] args){
+//         int U;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter Units used:");
+//         U=in.nextInt();
+//         if(U<=100){
+//             System.out.println(U+" rupees");
+//         }
+//         else if(U>=101&&U<=200){
+//             System.out.println(U*2+" rupees");
+//         }
+//         else if(U>=201&&U<=300){
+//             System.out.println(U*3+" rupees");
+//         }
+//         else if(U>300){
+//             System.out.println(U*5+" rupees");
+//         }
+        
+//     }
+// }
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String [] args){
+//         int S,P,H;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter Salary");
+//         S=in.nextInt();
+//         System.out.println("Enter Performance");
+//         P=in.nextInt();
+//         if(P>=90){
+//             H=(15*S)/100;
+//             System.out.println(S+H);
+//         }
+//         else if(P>=80){
+//             H=(12*S)/100;
+//             System.out.println(S+H);;
+//         }
+//         else if(P>=70){
+//             H=(8*S)/100;
+//             System.out.println(S+H);
+//         }
+//         else if(P>=60){
+//             H=(5*S)/100;
+//             System.out.println(S+H);
+//         }
+//         else{
+//             System.out.println("NO hike");
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String [] args){
+//         int S;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter Speed:");
+//         S=in.nextInt();
+//         if(S<=60){
+//             System.out.println("NO FINE");
+//         }
+//         else if(S<=61&&S>=80){
+//             System.out.println("500 FINE");;
+//         }
+//         else if(S<=100&&S>=81){
+//             System.out.println("2000 FINE");
+//         }
+//         else if(S<=120&&S>=101){
+//             System.out.println("5000 FINE");
+//         }
+//         else if(S>120){
+//             System.out.println("LICENSE WILL BE SUSPENDED");
+//         }
+//     }
+// }
+
+
+
+
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String [] args){
+//         int a,b,c;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter a:");
+//         a=in.nextInt();
+//         System.out.println("Enter b:");
+//         b=in.nextInt();
+//         System.out.println("Enter c:");
+//         c=in.nextInt();
+//         if(a>b){
+//             if(a>c){
+//                 System.out.println(a+"is greater");
+//             }
+//             else{
+//                 System.out.println(c+".is greater");
+//             }
+//         }
+//         else{
+//             if (b>c) {
+//             System.out.println(b+"is greater");
+//             }
+//             else{
+//             System.out.println(c+"is greater");
+//             }
+//         }
+        
+        
+//     }
+// }
+
+
+//WORKOUT QUESTIONS
+
+//convert 4 number to ascii letters
+
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String [] args){
+//         int a,b,c,d;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter 4 digits");
+//         a=in.nextInt();
+//         b=in.nextInt();
+//         c=in.nextInt();
+//         d=in.nextInt();
+//         System.out.println((char)a);
+//         System.out.println((char)b);
+//         System.out.println((char)c);
+//         System.out.println((char)d);
+        
+        
+//     }
+// }
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String [] args){
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter the no. of litres to be filled:");
+//         double F,D;
+//         double mileage,HPK;
+//         F=in.nextInt();
+//         System.out.println("Enter the distance to be covered:");
+//         D=in.nextInt();
+        
+//         HPK =(F/D)*100;
+//         System.out.println("(Litres/100km)  "+HPK);
+//         double M1=D*0.6214;
+//         double G1=F*0.2642;
+//         double MPH=M1/G1;
+//         System.out.println("miles/gallons   "+MPH);
+        
+        
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String[] args) {
+//         int pi=100,pu=20,co=10;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter the number of Pizzas:");
+//         int p=in.nextInt();
+//         System.out.println("Enter the number of Puffs:");
+//         int pu1=in.nextInt();   
+//         System.out.println("Enter the number of Cold Drinks:");
+//         int c=in.nextInt();
+//         int total= (pi*p)+(pu*pu1)+(co*c);      
+//         System.out.println("Total cost is: "+total);    
+
+//     }
+// }
+
+
+
+
+// import java.util.Scanner;
+// public class main2301{
+//     public static void main(String[] args) {
+//         int C,E,M;
+//         Scanner in=new Scanner(System.in);
+//         System.out.println("Enter the no of Students placed in CSE:");
+//         C=in.nextInt();
+//         System.out.println("Enter the no of Students placed in ECE:");
+//         E=in.nextInt();
+//         System.out.println("Enter the no of Students placed in MECH:");
+//         M=in.nextInt();
+//         if(C<0||E<0||M<0){
+//             System.out.println("Input is Invalid");
+//         }
+//         else if(C>E&&C>M){
+//             System.out.println("highest placement CSE");
+//         }
+//         else if(E>C&&E>M){
+//             System.out.println("highest placement ECE ");
+//         }
+//         else if(M>C&&M>E){
+//             System.out.println("highest placement MECH");
+//         }
+//         else{
+//             System.out.println("None of the department has highest placement");
+//         }   
+            
+
+//     }
+// }
+
+import java.util.Scanner;
+public class main2301{
+    public static void main(String[] args) {
+        int m;
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter month");
+        m=in.nextInt();
+        if(m>12||m<0){
+            System.out.println("invalid month");
+        } 
+        else if(m==12&&m<=2){
+            System.out.println("Winter");
+        } 
+        else if(m>=3&&m<=5){
+            System.out.println("Spring");
+        } 
+        else if(m>=6&&m<=7){
+            System.out.println("Summer");
+        }     
+        else{
+        System.out.println("Autumn");    
+        }
+    }
+}
---
+++ sample21,22.1.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+class java{
+    public static void main(String args[]){
+        // int a=10;
+        // System.out.println(a++ +" "+ ++a +" "+ a);
+        // System.out.println(++a +" "+ a +" "+ a++);
+        // System.out.println(a++ +" "+ a +" "+ a++);
+
+        // boolean b;
+        // b=5>3;
+        // System.out.println(b);
+        // int v=12;
+        // int p=9;
+
+        // System.out.println((v<p)&(3>9));
+        // System.out.println((v<p)&&(3>9));
+        // System.out.println(1987*10-29);
+        // int x,y,z;
+        // x=20;
+        // y=9;
+        // z=(x>y)?x:y;
+        // System.out.println(z);
+        
+        // int num=30;
+        // String result=(num%2==0)?"EVEn":"ODD";
+        // System.out.println(result);
+
+        // int a1=23,b1=46, c1=28;
+        // int max=(a1<b1)?((a1>c1)?a1:c1):((b1>c1)?b1:c1);
+        // System.out.println(max);
+
+        // int s2=13;
+        // String sol=(s2>0)?"positive":(s2==0)?"zero":"negative"; 
+        // System.out.println(sol);
+
+        // int age=17;
+        // String ans =(age<18)?"Minor":"major";
+        // System.out.println(ans);
+
+        // int di=100;
+        // String hi=(di>999 && di<=9999)?"four digit number":"not a four digit number";
+        // System.out.println(hi);
+
+        // int w=423,e=526;
+        // System.out.println(w^e);
+
+        //////////AFTERNOON
+        /// 
+        /// 
+        // System.out.println('A'+'B'+"hai");
+        // System.out.println('A'+"B"+'C');
+        // System.out.println('A'+10+'B'+"hai");
+        // System.out.println(true+"false"+true);
+
+        // System.out.println("example"+(3+2)+5);
+        // System.out.println('A'+'B'+"hai");
+        // System.out.println('A'+'B'+"hai");
+        // System.out.println(2*5/3);
+        // System.out.println(10 + +10 * 2);
+        // System.out.println(10*2/4%3);
+        
+
+        // int m1=943;
+        // System.out.println(m1<<3);
+
+
+
+
+
+
+        //INSTRUCTIONS
+        Scanner hi=new Scanner(System.in);
+        System.out.println("Enter 2 numbers:");
+        int a,b,c;
+        a=hi.nextInt();
+        b=hi.nextInt();
+        c=a+b;
+        System.out.println("sum:"+c);
+
+        System.out.println("Enter radius:");
+        float r=hi.nextFloat();
+        float area=3.14f*r*r;
+        System.out.println("Area of circle: "+area);
+
+        System.out.println("Enter celsius");
+        float celsius=hi.nextFloat();
+        float fahrenheit=(celsius*9/5)+32;  
+        System.out.println("Fahrenheit: "+fahrenheit);
+
+        System.out.println("Enter Fahrenheit:");
+        float fah=hi.nextFloat();
+        float c2=(fah-32)*5/9;
+        System.out.println("Celsius: "+c2);
+
+        System.out.println("Enter length of triangle:");
+        float l1=hi.nextFloat();
+        System.out.println("Enter breadth of triangle:");
+        float b1=hi.nextFloat(); 
+        System.out.print("Enter height of triangle:");
+        float h1=hi.nextFloat();
+        System.out.println("permiter of triangle:"+(l1+b1+h1));
+
+        
+
+
+        
+
+        
+       
+        
+         
+    }
+}
